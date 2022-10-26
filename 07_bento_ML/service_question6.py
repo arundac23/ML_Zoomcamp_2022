e77635a4a577f2b59fed6b1089b6d6d1d16efffa
Arun Dhanapalan <arun.dhanapalan@toyota.com>
2022-10-25 23:22:13 -0400

    week 7

---
+++ 07_bento_ML/service_question6.py
@@ -0,0 +1,18 @@
+import bentoml
+from bentoml.io import JSON, NumpyNdarray 
+import numpy as np
+
+## model from curl -O https://s3.us-west-2.amazonaws.com/bentoml.com/mlzoomcamp/coolmodel.bentomodel
+#model_ref = bentoml.sklearn.get("mlzoomcamp_homework:qtzdz3slg6mwwdu5")
+model_ref = bentoml.sklearn.get("mlzoomcamp_homework:jsi67fslz6txydu5")
+
+
+model_runner = model_ref.to_runner()
+
+svc = bentoml.Service("HW07_prediction", runners=[model_runner])
+
+@svc.api(input=NumpyNdarray(shape=(1, 4), dtype=np.float32, enforce_shape=True), output=JSON())
+async def classify(vector):
+    prediction = model_runner.predict.run(vector)
+
+    return prediction
